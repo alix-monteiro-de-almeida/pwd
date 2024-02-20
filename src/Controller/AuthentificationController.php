2a8512692339d8eae347df4fea76d6ed815b1b9f
alixunderscore <alix.platier-favier@laplateforme.io>
2024-02-20 12:16:10 +0100

    Merge diane/alix

--- src/Controller/AuthentificationController.php
+++ src/Controller/AuthentificationController.php
@@ -22,10 +22,14 @@ class AuthentificationController {
             !empty($email) &&
             !empty($password) 
         ) {
-            $fullname = isset($_POST['fullname']) ? trim($_POST['fullname']) : '';
+            $fullname = isset($_POST['fullname']) ? trim($_POST['fullname']) : ''; 
+            // condition ternaire 
+
             $email = isset($_POST['email']) ? trim($_POST['email']) : '';
             $password = isset($_POST['password']) ? trim($_POST['password']) : '';
             $role = ['ROLE_USER'];
+
+            // $toto = true ?: false; 
 
             $user = new User();
             $user->setFullname($fullname);
